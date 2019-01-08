8a64e06fab63e586673c621b2e74cabab7f5b37e
xiaopeng-pyhon <wuxiaoyanhenni@163.com>
2019-01-08 16:20:15 +0800

    201901081620

--- booktest/views.py
+++ booktest/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from booktest.models import BookInfo
+from django.http import HttpResponse
+from django.template import loader,RequestContext
+
 
 # Create your views here.
 
 def index(request):
-    books = BookInfo.objest.all()
-
+    books = BookInfo.objects.all()
     return render(request,'booktest/index.html',{'books': books})
