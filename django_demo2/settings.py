570c0a572863b7dff9d18417fa6df6da419e013b
xiaopeng-pyhon <wuxiaoyanhenni@163.com>
2019-01-15 17:10:48 +0800

    201901151715

--- django_demo2/settings.py
+++ django_demo2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_q5u114bq=hc@i-ny3@lh5^0^2hj4q^hk(#ur85wpl@-$to@(o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['39.105.128.207','127.0.0.1']
 
